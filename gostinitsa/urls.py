21f6cbaeeaf9576f86621711f60c4b2556e359ca
Степан <moonsightofficial@gmail.com>
2023-10-11 18:23:24 +0300

    all ok

--- gostinitsa/urls.py
+++ gostinitsa/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from gostinitsa import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about)
+    path('', views.index, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- gostinitsa/views.py
+++ gostinitsa/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Страница приложения гостиница')
+    return render(request, 'gostinitsa/index.html')
 def register(request):
-    return HttpResponse('Страница приложения гостиница')
+        return render(request,'gostinitsa/register.html')
 def login(request):
-    return HttpResponse('Страница приложения гостиница')
+    return render(request,'gostinitsa/login.html')
 def about(request):
-    return HttpResponse('Страница приложения гостиница')
+    return render(request,'gostinitsa/about.html')
