6527dd5027bc7cb9c0ac6ae0b8c6d555f61225a5
MSight1 <moonsightofficial@gmail.com>
2023-10-12 00:40:26 +0300

    123

--- gostinitsa/views.py
+++ gostinitsa/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'gostinitsa/index.html')
 def register(request):
-        return render(request,'gostinitsa/register.html')
+        return render(request,'gostinitsa/registertest.html')
 def login(request):
     return render(request,'gostinitsa/login.html')
 def about(request):
