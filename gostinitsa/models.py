88447bdd11602bbd80b6d967e6d0285e5ae8a1cd
Степан <moonsightoffcial@gmail.com>
2023-11-13 01:49:40 +0300

    Свободные и занятые номера

--- gostinitsa/models.py
+++ gostinitsa/models.py
@@ -1,34 +1,48 @@
 from django.db import models
 from django.urls import reverse
 
-class PublishedManager(models.Manager):  #класс для отображения только тех гостиниц, которые опубликованы
+class PublishedManager(models.Manager): #
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Gostinitsa.Status.PUBLISHED)
+
+class  StatusManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_status=Room.Status.AVAILABLE)
+
 class Gostinitsa(models.Model):
     class Status(models.IntegerChoices):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=255) #название
+    title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True) #текст про гостиницу ( ну типа вид создать )
-    time_create=models.DateTimeField(auto_now_add=True)
-    time_update=models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices,default=Status.DRAFT)
-    object = models.Manager() #чтобы Gostinitsa.objects продолжали работать
-    published = PublishedManager() #для работы класса отображения гостиниц
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('gos', kwargs={'gos_slug':self.slug})
+        return reverse('gos', kwargs={'gos_slug': self.slug})
+
 class Room(models.Model):
     class Status(models.IntegerChoices):
-        DRAFT = 0, 'Занят'
-        PUBLISHED = 1, 'Свободен'
+        OCCUPIED = 0, 'Занят'
+        AVAILABLE = 1, 'Свободен'
+
     hotel = models.ForeignKey(Gostinitsa, on_delete=models.CASCADE)
     room_number = models.CharField(max_length=100)
-    open = models.BooleanField(choices=Status.choices,default=Status.PUBLISHED)
+    is_status = models.IntegerField(choices=Status.choices, default=Status.AVAILABLE)
+
+    objects = models.Manager()
+    status = StatusManager()
+
     def __str__(self):
         return self.room_number
+
 
