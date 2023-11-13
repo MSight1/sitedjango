224dfb9a63a9dc119129ce01c6beb98cafeab3a4
Степан <moonsightoffcial@gmail.com>
2023-11-14 01:52:02 +0300

    Накидал админ панель, но не до конца

--- gostinitsa/admin.py
+++ gostinitsa/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Gostinitsa, Room
+
 
 # Register your models here.
+
+@admin.register(Gostinitsa)
+class GostinitsaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display_links = ('id', 'title')
+    ordering = ['time_create','title']
+    list_editable = ['is_published']
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'room_number', 'hotel', 'is_status')
+    list_display_links = ('id', 'room_number')
+    ordering = ['room_number','is_status']
+    list_editable = ['is_status', ]
